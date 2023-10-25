89ada89668e0efb8ccb2e7231ff275c2e451c1a5
Thomas Tahk <tomtahk27@gmail.com>
2023-10-25 14:22:48 -0700

    taking some form inputs to generate words but generation feature not working yet.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # word-of-the-day
 originally intended to be a single word of the day app
-but... steering this towards vocab generator
+but the name is misnomer compared to what this will turn out to be.
+For now, trying to put together some semblance of the most basic features I'd like.
+Currently stuck with how to handle submit event and have that result in additional html code below the form.
--- src/App.js
+++ src/App.js
@@ -1,29 +1,116 @@
 import "./styles.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import satvocab from "./freesatvocab.json";
 
-export default function App() {
-  // console.log(typeof satvocab);
-  const wordlist = Object.entries(satvocab);
-  const [isNewDay, setIsNewDay] = useState(false);
-  // console.log(wordlist);
-  // console.log(Array.isArray(wordlist));
-
-  const NewRandomWord = (isNewDay) => {
-    if (isNewDay) {
-      const random_word = wordlist[Math.floor(Math.random() * wordlist.length)];
-      console.log(random_word);
-      console.log(Array.isArray(random_word));
-      console.log(Object.entries(random_word[1]));
-      return random_word[1];
-    } else return "";
+// initial input values
+const initialValues = {
+    wordnum: "",
+    infotype: "word"
   };
+
+
+export default function App() {
+  const wordlist = Object.entries(satvocab);
+  // console log to determine what that wordlist actually looks like:
+  // console.log(wordlist);
+  // console.log(wordlist.length);
+  // console.log(wordlist[1]);
+  // console.log(wordlist[1][1].word);
+
+
+  const [values, setValues] = useState(initialValues);
+  const [result, setResult] = useState();
+
+  const handleWordChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  }
+
+  function wordDisplay(data) {
+    const wordnum = data.wordnum;
+    // const random_word = wordlist[Math.floor(Math.random() * wordlist.length)];
+    const wordSet = wordlist.slice(0, wordnum);
+    const infotype = data.infotype;
+    let displayWords = "";
+    if (infotype === "word") {
+      displayWords = wordSet.map(([num, word]) =>
+        <li key={num}>{word.word}</li>
+      );
+    } else if (infotype === "def") {
+      displayWords= wordSet.map(([num, word]) =>
+        <li key={num}>{word.definition}</li>
+      );
+    } else {
+      displayWords = wordSet.map(([num, word]) =>
+        <li key={num}>
+          {word.word}
+          <br></br>
+          {word.definition}
+        </li>
+      );
+    }
+    return (
+      <ul>{displayWords}</ul>
+    );
+  }
+  const submitHandler = (e) => {
+    wordDisplay(values);
+    e.preventDefault();
+  };
+
+
 
   return (
     <div className="App">
-      <h1>Your word of the day is: {NewRandomWord(true).word}</h1>
-      <p>Is it a new day?: {isNewDay ? "yes" : "no"}</p>
-      <p>{}</p>
+      <form>
+        <input
+          value={values.wordnum}
+          onChange={handleWordChange}
+          name="wordnum"
+          label="numberofwords"
+          placeholder="Enter Number of Words"
+        />
+        <div>
+          <h3>Vocab Display Options:</h3>
+          <label>
+            <input
+              type="radio"
+              name="infotype"
+              value="word"
+              checked={values.infotype === "word"}
+              onChange={handleWordChange}
+            />
+            Word Only
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="infotype"
+              value="def"
+              checked={values.infotype === "def"}
+              onChange={handleWordChange}
+            />
+            Definition Only
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="infotype"
+              value="both"
+              checked={values.infotype === "both"}
+              onChange={handleWordChange}
+            />
+            Both
+          </label>
+        </div>
+
+        <button type="submit" onClick={submitHandler}> Get Vocab </button>
+      </form>
     </div>
   );
 }
